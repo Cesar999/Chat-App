dbe87b8f7933f6c1979fa11f3949d54d29d65278
Cesar Encinas <cesar_rer@hotmail.com>
2018-11-14 15:15:49 -0800

    Moment locale internacionalization

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -2,11 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import * as moment from 'moment';
 
+moment.locale(localStorage.getItem('locale'));
+
 @Pipe({
   name: 'datefrom',
   pure: false
 })
-
 export class DatePipe implements PipeTransform {
   transform(value: string): any {
       return  moment(value).fromNow();
